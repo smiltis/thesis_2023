978e3466138109b37e1d4c45ea3429e4d57c804b
Smiltis <smiltis.zilinskas@ru.nl>
2023-04-29 17:46:54 +0200

    r-squared

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold
@@ -36,6 +37,76 @@ def plot_3D_example(x,y,z, title):
   ax.set_ylabel('Y Axis')
   ax.set_zlabel('Z Axis')
   ax.set_title(title)
+ 
+  # Show the plot
+  plt.show()
+
+def plotPredictedAndTest(y_test, y_pred, title):
+  # Generate time data
+  time = np.arange(0, y_test.shape[0]*9.8, 9.8) 
+
+  # Create a figure with x subplots
+  fig, axes = plt.subplots(y_test.shape[1], 1, figsize=(16, y_test.shape[1]*3))
+  
+  count = 0
+  if title == 'hand-only model':
+    for variable in hand_only_variables:
+        
+      # Plot X vs. time in the for subplot
+      axes[count].plot(time, y_test[:,count], label='{} actual'.format(hand_only_variables[count]))
+      axes[count].plot(time, y_pred[:,count],  label='{} predicted'.format(hand_only_variables[count]))
+      axes[count].set_xlabel('Time (ms)')
+      axes[count].set_ylabel(hand_only_variables[count])
+      # axes[count].set_title('{} vs Time for '.format(hand_only_variables[count], title))
+      axes[count].set_title(''+hand_only_variables[count]+' vs Time for '+title+'')
+      axes[count].legend(loc='center right')
+
+      count = count + 1
+
+  elif title == 'whole-arm model':
+    for variable in whole_arm_variables:
+        
+      # Plot X vs. time in the for subplot
+      axes[count].plot(time, y_test[:,count], label='{} actual'.format(whole_arm_variables[count]))
+      axes[count].plot(time, y_pred[:,count],  label='{} predicted'.format(whole_arm_variables[count]))
+      axes[count].set_xlabel('Time (ms)')
+      axes[count].set_ylabel(whole_arm_variables[count])
+      # axes[count].set_title('{} vs Time for {}'.format(whole_arm_variables[count], title))
+      axes[count].set_title(''+whole_arm_variables[count]+' vs Time for '+title+'')
+      axes[count].legend(loc='center right')
+
+      count = count + 1
+
+  elif title == 'egocentric model':
+    for variable in egocentric_variables:
+        
+      # Plot X vs. time in the for subplot
+      axes[count].plot(time, y_test[:,count], label='{} actual'.format(egocentric_variables[count]))
+      axes[count].plot(time, y_pred[:,count],  label='{} predicted'.format(egocentric_variables[count]))
+      axes[count].set_xlabel('Time (ms)')
+      axes[count].set_ylabel(egocentric_variables[count])
+      # axes[count].set_title('{} vs Time for {}'.format(egocentric_variables[count], title))
+      axes[count].set_title(''+egocentric_variables[count]+' vs Time for '+title+'')
+      axes[count].legend(loc='center right')
+
+      count = count + 1
+
+  elif title == 'joint-angles model':
+    for variable in joint_angles_variables:
+        
+      # Plot X vs. time in the for subplot
+      axes[count].plot(time, y_test[:,count], label='{} actual'.format(joint_angles_variables[count]))
+      axes[count].plot(time, y_pred[:,count],  label='{} predicted'.format(joint_angles_variables[count]))
+      axes[count].set_xlabel('Time (ms)')
+      axes[count].set_ylabel(joint_angles_variables[count])
+      # axes[count].set_title(''+joint_angles_variables[count]+' vs Time for '+title+'')
+      axes[count].set_title(''+joint_angles_variables[count]+' vs Time for '+title+'')
+      axes[count].legend(loc='center right')
+
+      count = count + 1
+        
+  # Adjust spacing between subplots
+  plt.subplots_adjust(hspace=2)
 
   # Show the plot
   plt.show()
@@ -69,54 +140,47 @@ def plot_joints():
   ax.set_title('Joint Angles during 9.8s of whole arm movement')
 
   # Add a legend
-  ax.legend()
+  ax.legend(loc='center right')
 
   # Show the plot
   plt.show()
 
 def ridge_regression(X, y, title):
-  # casting lists to np arrays, because with list it gives an error of indexing
-  X=np.array(X)
-  y=np.array(y)
-  alpha_values = np.linspace(0.1, 1, num=10) # alpha values from 0.1 till 3 by the step of 0.1
+    # casting lists to np arrays, because with list it gives an error of indexing
+    X=np.array(X)
+    y=np.array(y)
 
-  best_r_avg_square = -1
-  best_r_square_avg_alpha = -1
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-  for alpha in alpha_values:
-    # Creates a Ridge regression model
-    
-    ridge = Ridge(alpha=alpha) # alpha=regularization strength - 1 default value - test on multiple to find the best fit
+    # Specify custom alpha values
+    alphas = np.arange(0.1, 5.1, 0.1)
 
-    # Set up k-fold cross-validation
-    k = 4  # Number of folds
-    kf = KFold(n_splits=k, shuffle=True, random_state=42)
-    r_scores = []  # List to store R squared scores for each fold
+    # Define the RidgeCV model with cross-validation
+    ridge_cv = RidgeCV(alphas=alphas, cv=KFold(n_splits=5, shuffle=True, random_state=42))
 
-    # Perform k-fold cross-validation
-    for train_index, test_index in kf.split(X):
-      X_train, X_test = X[train_index], X[test_index]
-      y_train, y_test = y[train_index], y[test_index]
+    # Fit the RidgeCV model on the training set
+    ridge_cv.fit(X_train, y_train)
 
-      # Model fitting to the training data
-      ridge.fit(X_train, y_train)
+    # Get the best alpha value from RidgeCV
+    best_alpha = ridge_cv.alpha_
 
-      # Predict dependent variables based per model for test data
-      y_pred = ridge.predict(X_test)
-    
-      # Calculate R-squared for dependent variables predictions and append it to the r_scores array
-      r2 = r2_score(y_test, y_pred,  multioutput='variance_weighted')   # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.r2_score.html
-      r_scores.append(r2)
+    # Train the Ridge model with the best alpha on the entire training set
+    ridge_best = Ridge(alpha=best_alpha)
+    ridge_best.fit(X_train, y_train)
 
-    # Calculate average R squared value across all K-folds
-    avg_r2 = np.mean(r_scores)
+    # Evaluate the Ridge model on the testing set
+    y_pred = ridge_best.predict(X_test)
+    r2 = r2_score(y_test, y_pred,  multioutput='variance_weighted')
 
-    # print("R Squared score for each fold:", r_scores)
-    print("Average R Squared score across all",title,"K-folds with alpha value of", alpha,":", avg_r2)
-    if avg_r2 > best_r_avg_square:
-       best_r_avg_square = avg_r2
-       best_r_square_avg_alpha = alpha
-  print("Best Average R Squared score across all",title,"K-folds with alpha value of", best_r_square_avg_alpha,":", best_r_avg_square)
+    #Plot predicted and actual data
+    plotPredictedAndTest(y_test, y_pred, title)
+    print(y_test[0])
+    print(y_pred[0])
+
+    # Print the best alpha value and the R2 score on the testing set
+    print("Best Average R Squared score across all",title,"K-folds with alpha value of", best_alpha,":", r2)
+
 
 
 # This function receives an array of hand cartessian coordinates and transforms it to egocentric coordinates
@@ -141,13 +205,12 @@ def egocentric_coordinates(hand_position):
 
 # Beggining of code
 
-# print("The dictionary values: ", list(dict)[3:])
-# print(dict['jointNames'])
-
 firing_rates = dict['f_rates']
 elbow_position = dict['elbow_pos'] 
 hand_position = dict['hand_pos']
-joint_names = dict['jointNames']
+joint_angles_variables = []
+for variable in dict['jointNames'].flatten():
+   joint_angles_variables.append(variable[0])
 joint_angles = dict['joint_angle']
 
 hand_only = hand_position
@@ -158,19 +221,23 @@ egocentric = egocentric_coordinates(hand_position)
 # Z-score firing rates
 z_scored_firing_rates = scipy.stats.zscore(firing_rates)
 
+# Names of coordinate systems variables
+hand_only_variables = ['hand X','hand Y','hand Z']
+whole_arm_variables = ['hand X','hand Y','hand Z', 'elbow X','elbow Y','elbow Z']
+egocentric_variables = ['r', 'theta', 'phi']
 
 # Plotting example data
-# x = hand_position[:100,0].tolist()
-# y = hand_position[:100,1].tolist()
-# z = hand_position[:100,2].tolist()
-# plot_3D_example(x,y,z,'3D Scatterplot of hand position during 9.8s of movement')
+x = hand_position[:100,0].tolist()
+y = hand_position[:100,1].tolist()
+z = hand_position[:100,2].tolist()
+plot_3D_example(x,y,z,'3D Scatterplot of hand position during 9.8s of movement')
 
-# x = elbow_position[:100,0].tolist()
-# y = elbow_position[:100,1].tolist()
-# z = elbow_position[:100,2].tolist()
-# plot_3D_example(x,y,z,'3D Scatterplot of elbow position during 9.8s of movement')
+x = elbow_position[:100,0].tolist()
+y = elbow_position[:100,1].tolist()
+z = elbow_position[:100,2].tolist()
+plot_3D_example(x,y,z,'3D Scatterplot of elbow position during 9.8s of movement')
 
-# plot_joints()
+plot_joints()
 
 # Ridge regression for different models pass data to parameters
 
@@ -178,7 +245,8 @@ ridge_regression(z_scored_firing_rates,hand_position, 'hand-only model')
 ridge_regression(z_scored_firing_rates,whole_arm, 'whole-arm model')
 ridge_regression(z_scored_firing_rates,egocentric, 'egocentric model')
 ridge_regression(z_scored_firing_rates,joint_angles, 'joint-angles model')
-# print()
+
+
 
 
 
